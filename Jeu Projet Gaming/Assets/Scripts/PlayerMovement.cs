49fbec29942f6c8e80eb08f3a64ba6f092f831ca
Griboux Louis <louis.griboux@gmail.com>
2026-03-11 13:57:15 +0100

    changement sprint

--- Jeu Projet Gaming/Assets/Scripts/PlayerMovement.cs
+++ Jeu Projet Gaming/Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,34 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     float _horizontalMovement;
-    float speed = 3f;
+    float Normalspeed = 3f;
+    float SprintSpeed = 6f;
+    float _currentspeed;
     Rigidbody2D _rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
     float jumpingPower = 5f;
-    float acceleration = 6f;
+   
     private bool buttonPressed = false;
     private bool isJumping;
-    float _currentspeed;
+    
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        Debug.Log("Awake : Rigidbody2D initialized");
+    
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         buttonPressed = context.performed;
-        Debug.Log("Move called - buttonPressed: " + buttonPressed);
         if (buttonPressed)
         {
             float moveInput = context.ReadValue<Vector2>().x;
-            Debug.Log("Move input x: " + moveInput);
+          
             if (moveInput != 0)
             {
-                _horizontalMovement = Mathf.Sign(moveInput);
-                Debug.Log("Horizontal movement set to: " + _horizontalMovement);
+                _horizontalMovement = Mathf.Sign(moveInput)
             }
         }
     }
@@ -68,26 +68,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if (groundCheck == null)
         {
-            Debug.Log("IsGrounded - groundCheck is null");
             return false;
         }
         Collider2D collider = Physics2D.OverlapCircle(groundCheck.position, 0.15f, groundLayer);
         bool grounded = collider != null;
-        Debug.Log("IsGrounded - grounded: " + grounded + " | groundCheck position: " + groundCheck.position);
+       
         return grounded;
     }
 
 
 
 
-    private void Sprint(){
-        if(Input.GetKey(Key.code.LeftShift))
+    private void Sprint(InputAction.CallbackContext context){
+        if(context.performed)
             {
-                speed = acceleration;
+             _currentspeed = SprintSpeed;
             }
-        else{
-                _currentspeed = speed;
+        if (context.canceled){
+            _currentSpeed = _baseSpeed;
         }
+}
     }
     
-}
+
